a64909d8ec1622cf8299e3c87b3a9e513acdc780
Hisato Shoji <francais.harry@gmail.com>
2016-05-03 17:53:12 +0900

    Fixed ISO8601 date formatter to use UTC always

--- libLifeLog/src/main/java/com/sonymobile/lifelog/utils/ISO8601Date.java
+++ libLifeLog/src/main/java/com/sonymobile/lifelog/utils/ISO8601Date.java
@@ -5,6 +5,7 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
+import java.util.TimeZone;
 
 /**
 
@@ -17,9 +18,9 @@ public final class ISO8601Date {
     /** Transform Calendar to ISO 8601 string. */
     public static String fromCalendar(final Calendar calendar) {
         Date date = calendar.getTime();
-        String formatted = new SimpleDateFormat("YYYY-MM-DD’T’hh:mm:ss.sssTZD")
-                .format(date);
-        return formatted.substring(0, 27) + ":" + formatted.substring(27);
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+        formatter.setTimeZone(TimeZone.getTimeZone("UTC"));
+        return formatter.format(date);
     }
 
     /** Get current date and time formatted as ISO 8601 string. */
